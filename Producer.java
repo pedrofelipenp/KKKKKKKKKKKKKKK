8595b470d73273f365bd8c22685c0bbe57043e0a
Kayan Barreto <129319533+Kayanbarreto@users.noreply.github.com>
2026-03-10 14:41:21 -0300

    Implement Producer class for producing items

---
+++ Producer.java
@@ -0,0 +1,30 @@
+public class Producer implements Runnable {
+
+    private final Buffer buffer;
+    private final int sleepTime;
+    private final int id;
+
+    public Producer(int id, Buffer buffer, int sleepTime) {
+        this.id = id;
+        this.buffer = buffer;
+        this.sleepTime = sleepTime;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(sleepTime);
+
+                int item = (int) (Math.random() * 100);
+
+                System.out.println("Producer " + id + " produced item " + item);
+
+                buffer.put(item);
+
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
